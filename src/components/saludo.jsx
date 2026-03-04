383f83098f8e38d25887d40c94bdb8faaed51333
samuel <samueltxl201@gmail.com>
2026-03-04 07:35:21 -0500

    Solucion Ejemplo 1

---
+++ src/components/saludo.jsx
@@ -0,0 +1,13 @@
+// Archivo: Saludo.jsx
+// 1. Creamos la función (nuestro bloque de LEGO)
+function Saludo() {
+// 2. Devolvemos lo que queremos que se vea en pantalla
+return (
+<div>
+<h1>¡Hola, Universo!</h1>
+<p>Este es mi primer bloque de LEGO en React.</p>
+</div>
+);
+}
+// 3. Lo exportamos para usarlo en nuestra nave espacial (App principal)
+export default Saludo;
